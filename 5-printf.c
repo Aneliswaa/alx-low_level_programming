e204f1f7fc9329212fde6f72afdc1e4eb153baf8
Anele <anelenene04@gmail.com>
2023-07-13 14:56:37 -0700

    last one

--- 5-printf.c
+++ 5-printf.c
@@ -1,3 +1,4 @@
+#!/bin/bash
 #include <stdio.h>
 /**
  * main - A program that prints the printf function
@@ -5,6 +6,6 @@
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
-return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
